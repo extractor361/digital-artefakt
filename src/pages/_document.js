4518aa4ec2ca5bf8f375f1909061b71ab91c3f78
extract36111 <digitalartefakt361@gmail.com>
2025-08-22 15:25:11 +0200

    favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        {/* Ako imaš PNG verziju */}
+        {/* <link rel="icon" type="image/png" href="/favicon.png" /> */}
+        {/* Za Apple/ios support */}
+        {/* <link rel="apple-touch-icon" href="/favicon.png" /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
