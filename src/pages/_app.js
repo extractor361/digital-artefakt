2904e9224951b4d197011b9520beb951bf3508d8
extract36111 <digitalartefakt361@gmail.com>
2025-10-09 22:48:27 +0200

    prevod

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import useMagneticHover from "@/hooks/useMagneticHover";
 import Script from "next/script";
+import Preloader from "@/components/common/Preloader";
 
 import "../../public/assets/css/bootstrap-icons.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -11,16 +12,15 @@ import "../../public/assets/css/preloader.css";
 import "../../public/assets/css/animate.min.css";
 import "../../public/assets/css/style2.css";
 import "node_modules/react-modal-video/css/modal-video.css";
-import Preloader from "@/components/common/Preloader";
 
 export default function App({ Component, pageProps }) {
-  const [loading, setLoading] = useState(true); // start with true
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Run client-only hooks
+    // Client-only hook
     useMagneticHover();
 
-    // Import bootstrap JS dynamically
+    // Import bootstrap JS
     import("bootstrap/dist/js/bootstrap");
 
     // Delay preloader for 3 seconds
@@ -30,37 +30,35 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
-      {loading ? (
-        <Preloader />
-      ) : (
-        <>
-          <Component {...pageProps} />
+      {/* Preloader */}
+      {loading && <Preloader />}
 
-          {/* WOW.js */}
-          <Script
-            src="/js/wow.min.js"
-            strategy="afterInteractive"
-            onLoad={() => new WOW().init()}
-          />
+      {/* Main component */}
+      <Component {...pageProps} />
 
-          {/* Tawk.to */}
-          <Script id="tawk-to" strategy="afterInteractive">
-            {`
-              setTimeout(function() {
-                var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-                (function(){
-                  var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-                  s1.async = true;
-                  s1.src = 'https://embed.tawk.to/688f250171bfc61926225957/1j1nih47q';
-                  s1.charset = 'UTF-8';
-                  s1.setAttribute('crossorigin', '*');
-                  s0.parentNode.insertBefore(s1, s0);
-                })();
-              }, 5000);
-            `}
-          </Script>
-        </>
-      )}
+      {/* WOW.js */}
+      <Script
+        src="/js/wow.min.js"
+        strategy="afterInteractive"
+        onLoad={() => new WOW().init()}
+      />
+
+      {/* Tawk.to */}
+      <Script id="tawk-to" strategy="afterInteractive">
+        {`
+          setTimeout(function() {
+            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+            (function(){
+              var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+              s1.async = true;
+              s1.src = 'https://embed.tawk.to/688f250171bfc61926225957/1j1nih47q';
+              s1.charset = 'UTF-8';
+              s1.setAttribute('crossorigin', '*');
+              s0.parentNode.insertBefore(s1, s0);
+            })();
+          }, 5000);
+        `}
+      </Script>
     </>
   );
 }
