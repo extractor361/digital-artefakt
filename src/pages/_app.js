41ec7d131d84a2aa49c439ef4f3bc978124b1469
extract36111 <digitalartefakt361@gmail.com>
2025-10-09 22:19:04 +0200

    prevod

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import useMagneticHover from "@/hooks/useMagneticHover";
 import Script from "next/script";
+import { NextIntlProvider } from "next-intl";
 
 import "../../public/assets/css/bootstrap-icons.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -13,50 +14,48 @@ import "../../public/assets/css/style2.css";
 import "node_modules/react-modal-video/css/modal-video.css";
 import Preloader from "@/components/common/Preloader";
 
-
 export default function App({ Component, pageProps }) {
   const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     setLoading(false);
-    setTimeout(() => {
-      setLoading(true);
-    }, 3000);
+    setTimeout(() => setLoading(true), 3000);
   }, []);
+
   useMagneticHover();
+
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap");
   }, []);
-  
+
   return (
     <>
-
       {loading ? (
-        <>
+        <NextIntlProvider messages={pageProps.messages}>
           <Component {...pageProps} />
-          <Script id="wow" src="/js/wow.min.js">
-          
-          </Script>
-<Script id="tawk-to" strategy="afterInteractive">
-  {`
-    setTimeout(function() {
-      var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-      (function(){
-        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-        s1.async = true;
-        s1.src = 'https://embed.tawk.to/688f250171bfc61926225957/1j1nih47q';
-        s1.charset = 'UTF-8';
-        s1.setAttribute('crossorigin', '*');
-        s0.parentNode.insertBefore(s1, s0);
-      })();
-    }, 5000); // 5 sekundi delay
-  `}
-</Script>
 
-          <Script
-            id="initWow"
-            strategy="lazyOnload"
-          >{`new WOW().init();`}</Script>
-        </>
+          <Script id="wow" src="/js/wow.min.js" />
+
+          <Script id="tawk-to" strategy="afterInteractive">
+            {`
+              setTimeout(function() {
+                var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+                (function(){
+                  var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+                  s1.async = true;
+                  s1.src = 'https://embed.tawk.to/688f250171bfc61926225957/1j1nih47q';
+                  s1.charset = 'UTF-8';
+                  s1.setAttribute('crossorigin', '*');
+                  s0.parentNode.insertBefore(s1, s0);
+                })();
+              }, 5000);
+            `}
+          </Script>
+
+          <Script id="initWow" strategy="lazyOnload">
+            {`new WOW().init();`}
+          </Script>
+        </NextIntlProvider>
       ) : (
         <Preloader />
       )}
--- src/pages/en/index.js
+++ src/pages/en/index.js
@@ -1,67 +1,37 @@
-import Home3Team from "@/components/Team/Home3Team";
-import About3 from "@/components/about/About3";
-import Banner3 from "@/components/bannner/Banner3";
-import Home3Blog from "@/components/blog/Home3Blog";
-import Preloader from "@/components/common/Preloader";
-import Home3Contact from "@/components/contact/Home3Contact";
-import Footer3 from "@/components/footer/Footer3";
-import Header3 from "@/components/header/Header3";
-import Partner3 from "@/components/partner/Partner3";
-import PricingPlan3 from "@/components/pricingplan/PricingPlan3";
-import Solution3 from "@/components/solutions/Solution3";
-import SuccessStory3 from "@/components/successStorys/SuccessStory3";
-import Testimonial3 from "@/components/testimonial/Testimonial3";
-import useBodyClass from "@/hooks/useBodyClass";
-import Head from "next/head";
+import { useTranslations } from 'next-intl';
+import Head from 'next/head';
+import useBodyClass from '@/hooks/useBodyClass';
+
+import Header3 from '@/components/header/Header3';
+import Banner3 from '@/components/bannner/Banner3';
+import Partner3 from '@/components/partner/Partner3';
+import Solution3 from '@/components/solutions/Solution3';
+import About3 from '@/components/about/About3';
+import SuccessStory3 from '@/components/successStorys/SuccessStory3';
+import Testimonial3 from '@/components/testimonial/Testimonial3';
+import PricingPlan3 from '@/components/pricingplan/PricingPlan3';
+import Home3Contact from '@/components/contact/Home3Contact';
+import Footer3 from '@/components/footer/Footer3';
 
 export default function Home() {
+  const t = useTranslations(); // next-intl hook
   useBodyClass("home-dark2");
 
   return (
     <>
       <Head>
-  <title>Izrada veb sajtova i aplikacija | Digitalni marketing | Crna Gora, Bar </title>
-  <meta name="description" content="Nudimo vam profesionalnu izradu modernih i funkcionalnih web sajtova i aplikacija koje unapređuju korisničko iskustvo i podižu vaš brend na viši nivo. Naš tim kombinuje dizajn, razvoj i tehničku preciznost kako bi vam isporučio pouzdana digitalna rješenja prilagođena vašim ciljevima." />
-  <meta name="keywords" content="digitalni marketing, izrada sajtova, web aplikacije, SEO, Google Ads, društvene mreže, softverska rješenja, IT podrška, Crna Gora" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="icon" href="/assets/img/logo.png" />
-  {/* Open Graph (Facebook/LinkedIn) */}
-  <meta property="og:title" content="Digital Artefakt | Web razvoj i digitalni marketing" />
-  <meta property="og:description" content="Kompletna digitalna rješenja – web sajtovi, aplikacije, SEO, Google Ads, društvene mreže i više." />
-  <meta property="og:image" content="/assets/img/home-3/naslovna1.pn" />
-  <meta property="og:type" content="website" />
-  <meta property="og:url" content="https://www.digital-artefakt.me" />
-  <meta name="google-site-verification" content="FLR_6KLLRFz_z06X6n0tSmaZSb9PZuIBRx_-oDImE9Q" />
-
-  {/* JSON-LD struktura organizacije */}
-  <script
-    type="application/ld+json"
-    dangerouslySetInnerHTML={{
-      __html: JSON.stringify({
-        "@context": "https://schema.org",
-        "@type": "Organization",
-        name: "Digital Artefakt",
-        url: "https://www.digital-artefakt.me",
-        logo: "/assets/img/logo.svg",
-        address: {
-          "@type": "PostalAddress",
-          streetAddress: "Bulevar Revolucije C-7",
-          addressLocality: "Bar",
-          addressCountry: "ME"
-        },
-        contactPoint: {
-          "@type": "ContactPoint",
-          telephone: "+38268062361",
-          contactType: "customer service"
-        },
-        sameAs: [
-          "https://www.facebook.com/DigitalArtefakt",
-          "https://www.instagram.com/digital_artefakt/"
-        ]
-      })
-    }}
-  />
-</Head>
+        <title>{t('seo.title')}</title>
+        <meta name="description" content={t('seo.description')} />
+        <meta name="keywords" content={t('seo.keywords')} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/assets/img/logo.png" />
+        {/* Open Graph */}
+        <meta property="og:title" content={t('seo.ogTitle')} />
+        <meta property="og:description" content={t('seo.ogDescription')} />
+        <meta property="og:image" content="/assets/img/home-3/naslovna1.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.digital-artefakt.me" />
+      </Head>
 
       <Header3 />
       <Banner3 />
@@ -75,4 +45,13 @@ export default function Home() {
       <Footer3 />
     </>
   );
+}
+
+// next-intl getStaticProps za preuzimanje JSON fajla
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      messages: (await import(`../../translation/${locale}.json`)).default
+    }
+  };
 }
