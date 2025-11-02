6a9fc1f60d779ed6bd7f93020ef9a570f7dfda7b
extract36111 <digitalartefakt361@gmail.com>
2025-11-02 21:41:55 +0100

    prevod

--- src/pages/usluge/izrada-mobilnih-aplikacija.js
+++ src/pages/usluge/izrada-mobilnih-aplikacija.js
@@ -85,7 +85,7 @@ function ServiceDetailsPage() {
                 </p>
 
                 <div className="mt-4">
-                  <Link href="/kontakt" className="theme-btn">
+                  <Link href="/kontakt" className="primary-btn3">
                     Zatražite ponudu
                   </Link>
                 </div>
@@ -114,11 +114,58 @@ function ServiceDetailsPage() {
                 </div>
 
                 <div className="widget contact-widget">
-                  <h4>Kontaktirajte nas</h4>
-                  <p>Imate ideju za aplikaciju? Javite nam se i pretvorimo je u stvarnost.</p>
-                  <Link href="/kontakt" className="theme-btn">
-                    Kontakt
-                  </Link>
+                  <div className="contact-form-wrap">
+            <div className="form-tltle">
+              <h5>Make a Free Consulting</h5>
+            </div>
+            <div className="contact-form">
+              <form>
+                <div className="row">
+                  <div className="col-md-6 mb-20">
+                    <div className="form-inner">
+                      <label>first name</label>
+                      <input type="text" />
+                    </div>
+                  </div>
+                  <div className="col-md-6 mb-20">
+                    <div className="form-inner">
+                      <label>Last name</label>
+                      <input type="text" />
+                    </div>
+                  </div>
+                  <div className="col-lg-12 mb-20">
+                    <div className="form-inner">
+                      <label>Company/Organization</label>
+                      <input type="text" />      
+                    </div>
+                  </div>
+                  <div className="col-lg-12 mb-20">
+                    <div className="form-inner">
+                      <label>Email</label>
+                      <input type="email" />
+                    </div>
+                  </div>
+                  <div className="col-lg-12 mb-20">
+                    <div className="form-inner">
+                      <label>Phone</label>
+                      <input type="email" />
+                    </div>
+                  </div>
+                  <div className="col-lg-12 mb-20">
+                    <div className="form-inner">
+                      <label>Message</label>
+                      <textarea defaultValue={""} />
+                    </div>
+                  </div>
+                  <div className="col-lg-12">
+                    <div className="form-inner">
+                      <button className="primary-btn3" type="submit">Submit</button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
                 </div>
               </div>
             </div>
