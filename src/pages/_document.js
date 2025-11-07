447ab01373c90746db08b5c03d12062a09cda554
extract36111 <digitalartefakt361@gmail.com>
2025-11-07 18:55:45 +0100

    prevod

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="sr">
       <Head>
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
@@ -10,6 +10,22 @@ export default function Document() {
         {/* <link rel="icon" type="image/png" href="/favicon.png" /> */}
         {/* Za Apple/ios support */}
         {/* <link rel="apple-touch-icon" href="/favicon.png" /> */}
+
+        {/* ✅ Google Ads / Analytics tag */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17116231799"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17116231799');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
