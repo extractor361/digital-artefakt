3ad4af0ba6c6bc579a6165b1e63a813732c443d1
extract36111 <digitalartefakt361@gmail.com>
2025-11-10 18:29:55 +0100

    kalkulator

--- src/components/WebCostCalculator.js
+++ src/components/WebCostCalculator.js
@@ -84,7 +84,7 @@ export default function WebCostCalculator() {
   let totalMax = 0;
 
   const basePrices = {
-    prezentacioni: [300, 500],
+    prezentacioni: [299, 500],
     advokat: [350, 550],
     nekretnine: [500, 700],
     racunovodstvo: [400, 600],
