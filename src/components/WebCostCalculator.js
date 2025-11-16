4b5e559203db3650c3fbc6c728fedb4090da1f3e
extract36111 <digitalartefakt361@gmail.com>
2025-11-16 15:19:43 +0100

    cijena

--- src/components/WebCostCalculator.js
+++ src/components/WebCostCalculator.js
@@ -333,6 +333,8 @@ const LABELI = {
   // ✔ EMAIL SA PDF ATTACHMENTOM (FormSubmit)
   // --------------------------------------------------
   const emailData = new FormData();
+emailData.append("access_key", "d1ee36ec-b3d2-4929-8576-b8d47b957256");
+emailData.append("subject", "Nova web ponuda sa kalkulatora");
 
   emailData.append("Ime klijenta", klijent.ime);
   emailData.append("Adresa", klijent.adresa);
@@ -345,11 +347,11 @@ const LABELI = {
 
   emailData.append("attachment", pdfBlob, "ponuda.pdf");
 
-  emailData.append("_subject", "Nova web ponuda sa kalkulatora");
-  emailData.append("_captcha", "false");
-  emailData.append("_template", "table");
+  //emailData.append("_subject", "Nova web ponuda sa kalkulatora");
+  //emailData.append("_captcha", "false");
+  //emailData.append("_template", "table");
 
-  try {
+  {/*try {
     const res = await fetch("https://formsubmit.co/ajax/info@digital-artefakt.me", {
       method: "POST",
       body: emailData,
@@ -362,8 +364,25 @@ const LABELI = {
   } catch (err) {
     console.error("Greška prilikom slanja emaila:", err);
   }
-};
+};*/}
+try {
+  const res = await fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    body: emailData
+  });
 
+  const data = await res.json();
+  console.log("Web3Forms response:", data);
+
+  if (data.success) {
+    console.log("Ponuda uspješno poslata na email!");
+  } else {
+    console.log("Greška prilikom slanja ponude.");
+  }
+
+} catch (err) {
+  console.log("Greška prilikom slanja emaila:", err);
+}
 
   return (
     <div className={styles.kalkulatorSajta}>
