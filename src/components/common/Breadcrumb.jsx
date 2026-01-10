8fe3fdd47bab698e56906f75fcc88996b2e78e2d
extractor361 <selastan@gmail.com>
2026-01-10 22:56:03 +0100

    onama

--- src/components/common/Breadcrumb.jsx
+++ src/components/common/Breadcrumb.jsx
@@ -21,7 +21,7 @@ function Breadcrumb({ title, pageName, pageList }) {
             <div className="breadcrumb-wrapper">
               <div className="breadcrumb-cnt">
                 <span>{pageName}</span>
-                <h2 style={{color:"#dbdbdb"}}>"{title}"</h2>
+                <h2 style={{color:"#f3f3f3"}}>"{title}"</h2>
                 <div className="breadcrumb-list">
                   <Link legacyBehavior href="/"><a>{t("Naslovna")}</a></Link>
                   <img
